b75d37a713967ae0f852238e5684d8026f3614bf
Marcus Exner <marcus.exner@gmail.com>
2019-07-30 22:00:36 +0200

    Update code format.

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 const pgClient = new Pool({
     user: keys.pgUser,
     host: keys.pgHost,
@@ -48,21 +48,21 @@ app.get('/values/all', async (req, res) => {
 app.get('/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
         res.send(values);
-    })
+    });
 });
 
 app.post('/values', async (req, res) => {
     const index = req.body.index;
 
     if (parseInt(index) > 40) {
-        return res.status(422).send('Index to high');
+        return res.status(422).send('Index too high');
     }
 
     redisClient.hset('values', index, 'Nothing yet!');
     redisPublisher.publish('insert', index);
     pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    res.send({working: true});
+    res.send({ working: true });
 });
 
 app.listen(5000, err => {
