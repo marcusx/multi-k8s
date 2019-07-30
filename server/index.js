8fde064ca3917c66adc2cccda44fcac7073fb98d
Marcus Exner <marcus.exner@gmail.com>
2019-07-30 21:47:44 +0200

    Fixing select.

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient('SELECT * FROM values');
+    const values = await pgClient.query('SELECT * from values');
 
     res.send(values.rows);
 });
