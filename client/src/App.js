e6bb7ae19dc2a040a30e3d7cdcf80f6f8b449bfc
Marcus Exner <marcus.exner@gmail.com>
2019-07-30 17:05:21 +0200

    Fix Typo.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRoter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
